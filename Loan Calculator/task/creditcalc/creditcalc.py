97cc482a61020ea3a39f651b14eb3009bbed4f8d
rolmos14 <olmos.rafa@gmail.com>
2021-01-10 21:29:30 +0100

    Final version of loan calculator project

--- Loan Calculator/task/creditcalc/creditcalc.py
+++ Loan Calculator/task/creditcalc/creditcalc.py
@@ -1,71 +1,102 @@
 import math
 import argparse
+import sys
 
 
-def get_loan_principal():
-    return float(input("Enter the loan principal:\n"))
-
-
-def get_monthly_payment():
-    return float(input("Enter the monthly payment:\n"))
-
-
-def get_num_payments():
-    return int(input("Enter the number of periods:\n"))
-
-
-def get_monthly_interest():
-    annual_interest = float(input("Enter the loan interest:\n"))
+def monthly_interest(annual_interest):
     # Calculate monthly interest rate
     return annual_interest / 100 / 12
 
 
-parser = argparse.ArgumentParser(description="This program computes annuity and differentiated payments")
+def overpayment(p, **kwargs):
+    if kwargs.get("acc"):
+        return int(kwargs["acc"] - p)
+    else:
+        return int(kwargs["n"] * kwargs["A"] - p)
 
-parser.add_argument("--type", choices={"annuity", "diff"})
 
-what_to_calc = input('What do you want to calculate?\n' +
-                     'type "n" - for number of monthly payments,\n' +
-                     'type "a" - for annuity monthly payment amount,\n' +
-                     'type "p" - for loan principal:\n')
+# Create parser object and arguments
+parser = argparse.ArgumentParser(description="This program computes annuity and differentiated payments.")
+parser.add_argument("--type", choices=["annuity", "diff"], help="Type of payment to be calculated. \
+                                                                This parameter is always REQUIRED.")
+parser.add_argument("--payment", help="Monthly payment amount. Not compatible with --type=diff.")
+parser.add_argument("--principal", help="Loan principal amount.")
+parser.add_argument("--periods", help="Number of months needed to repay the loan.")
+parser.add_argument("--interest", help="Loan annual interest. This parameter is always REQUIRED.")
 
-if what_to_calc == "n":
-    P = get_loan_principal()
-    A = get_monthly_payment()
-    i = get_monthly_interest()
+# Get arguments and check for: missing, incompatibilities, number of parameters or negative values
+args = parser.parse_args()
+if args.type is None or args.interest is None or \
+        (args.type == "diff" and args.payment is not None) or \
+        len(sys.argv) < 5:
+    print("Incorrect parameters")
+    exit()
+for arg in args.__dict__.values():
+    if arg != "annuity" and arg != "diff" and arg is not None:
+        if float(arg) < 0:
+            print("Incorrect parameters")
+            exit()
 
-    # Calculate number of months
-    n = math.ceil(math.log(A / (A - i * P), 1 + i))
+# Declare loan variables if corresponding parameter is present
+if args.principal is not None:
+    P = float(args.principal)
+if args.periods is not None:
+    n = int(args.periods)
+i = monthly_interest(float(args.interest))  # interest is always required
+if args.payment is not None:
+    A = int(args.payment)
 
-    # Convert to years and months
-    years = n // 12
-    months = n % 12
-    year_str = "year" if years < 2 else "years"
-    month_str = "months" if months > 1 else "month"
-    print("It will take ",
-          f"{years} {year_str} and " if years != 0 else "",
-          f"{months} {month_str} " if months != 0 else "",
-          "to repay this loan!",
-          sep="")
+# Calculate differentiated payments
+if args.type == "diff":
 
-elif what_to_calc == "a":
-    P = get_loan_principal()
-    n = get_num_payments()
-    i = get_monthly_interest()
+    # Calculate months 1st to nth
+    accumulated_payments = 0
+    P_n = P / n
+    for m in range(1, n + 1):
+        D = math.ceil(P_n + i * (P - (P * (m - 1) / n)))
+        print(f"Month {m}: payment is {D}")
+        accumulated_payments += D
 
-    # Calculate monthly payment
-    A = math.ceil(P * i * (1 + i) ** n / ((1 + i) ** n - 1))
-    last_payment = P - (n - 1) * A
-    print(f"Your monthly payment = {A}",
-          " and the last payment = " + str(last_payment) if last_payment != A and last_payment > 0 else "",
-          "!",
-          sep="")
+    # Calculate overpayment
+    print(f"\nOverpayment = {overpayment(P, acc=accumulated_payments)}")
 
-else:  # what_to_calc == "p"
-    A = get_monthly_payment()
-    n = get_num_payments()
-    i = get_monthly_interest()
+# Calculate annuity payment
+else:  # args.type == "annuity"
+    if args.principal is not None:
+        if args.periods is not None:
 
-    # Calculate loan principal
-    P = A / (i * (1 + i) ** n / ((1 + i) ** n - 1))
-    print(f"Your loan principal {round(P)}!")
+            # Calculate annuity payment
+            A = math.ceil(P * i * (1 + i) ** n / ((1 + i) ** n - 1))
+            print(f"Your annuity payment = {A}!")
+
+            # Calculate overpayment
+            print(f"\nOverpayment = {overpayment(P, n=n, A=A)}")
+
+        else:  # args.payment is not None
+
+            # Calculate number of months
+            n = math.ceil(math.log(A / (A - i * P), 1 + i))
+
+            # Convert to years and months
+            years = n // 12
+            months = n % 12
+            year_str = "year" if years < 2 else "years"
+            month_str = "months" if months > 1 else "month"
+            print("It will take",
+                  f" {years} {year_str} " if years != 0 else "",
+                  "and" if years != 0 and months != 0 else "",
+                  f" {months} {month_str} " if months != 0 else "",
+                  "to repay this loan!",
+                  sep="")
+
+            # Calculate overpayment
+            print(f"\nOverpayment = {overpayment(P, n=n, A=A)}")
+
+    else:  # args.payment is not None
+
+        # Calculate loan principal
+        P = A / (i * (1 + i) ** n / ((1 + i) ** n - 1))
+        print(f"Your loan principal {round(P)}!")
+
+        # Calculate overpayment
+        print(f"\nOverpayment = {overpayment(P, n=n, A=A)}")
